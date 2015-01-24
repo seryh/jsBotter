03b1bd123fe04bfd5fdd9a1bd145ccc6c9712af0
Oleg Seryh <o.seryh@gmail.com>
2015-01-24 22:21:15 +0600

    pushbullet pack add

---
+++ botPlugins/push.js
@@ -0,0 +1,13 @@
+/**
+ * pushbullet plugin
+ *
+ * @type {exports}
+ */
+
+var PushBullet = require('pushbullet');
+
+module.exports = {
+    'message': function(nick, target, message, raw) {
+
+    }
+};
