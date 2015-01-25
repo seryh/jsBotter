2125617b2f746a738081288b289ea3035ea3a37e
Oleg Seryh <o.seryh@gmail.com>
2015-01-25 20:05:07 +0600

    pushbullet plugin work process

--- botPlugins/push.js
+++ botPlugins/push.js
@@ -17,24 +17,58 @@ var _regPushToken = function(client, nick, message, raw) {
     if (commandArr === null) return false;
         token = commandArr[2] || null;
 
-    var model = new pushbulletModel();
+    pushbulletModel.findOne({networkName: client._networkName, nick: nick },
+        function (err, _model) {
+            if (err) {
+                client.say(nick, "Произошла ошибка "+JSON.stringify(err));
+                return false;
+            }
 
-    model.networkName = client._networkName;
-    model.nick = nick;
-    model.token = token;
-    model.vhost =  util.format('%s@%s', raw.user, raw.host) ;
-    model.date = new Date();
+            var model;
 
+            if (Boolean(_model) === false) {
+                model = new pushbulletModel();
+            } else {
+                model = _model;
+            }
 
-    //todo поиск по хосту/нику чтобы 1 токен на одного юзера
-    model.save(function (err) {
-        if (err) {
+            model.networkName = client._networkName;
+            model.nick = nick;
+            model.token = token;
+            model.vhost =  util.format('%s@%s', raw.user, raw.host);
+            model.date = new Date();
 
-            return false;
-        }
-        client.say(nick, "API key для pushbullet зарегестрирован");
-    });
+            model.save(function (err2) {
+                if (err2) {
+                    client.say(nick, "Произошла ошибка "+JSON.stringify(err2));
+                    return false;
+                }
+                client.say(nick, "API key для pushbullet успешно зарегестрирован");
+            });
+        });
+};
 
+var _getPushToken = function(client, nick, message, raw) {
+    var re = /^(!getpush)/gi,
+        commandArr = re.exec(message),
+        vhost = vhost =  util.format('%s@%s', raw.user, raw.host);
+
+    if (commandArr === null) return false;
+
+    pushbulletModel.findOne({networkName: client._networkName, nick:nick, vhost:vhost },
+        function (err, _model) {
+            if (err) {
+                client.say(nick, "Произошла ошибка "+JSON.stringify(err));
+                return false;
+            }
+
+            if (Boolean(_model) !== false) {
+                client.say(nick, "Ваш токен - " + _model.token);
+            } else {
+                client.say(nick, "Токен не найден, или у вас нет доступа по vhost");
+            }
+
+        });
 };
 
 module.exports = {
@@ -44,6 +78,7 @@ module.exports = {
     'pm': function(nick, message, raw) {
 
         _regPushToken(this, nick, message, raw);
+        _getPushToken(this, nick, message, raw);
 
 
     }
