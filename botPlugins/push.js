9308755d0971a0fd5052f2cd128772fb41e797a3
Oleg Seryh <o.seryh@gmail.com>
2015-01-25 20:32:02 +0600

    pushbullet plugin work process

--- botPlugins/push.js
+++ botPlugins/push.js
@@ -9,6 +9,25 @@ var PushBullet = require('pushbullet');
 var pushbulletModel = require('../models/pushbullet'),
     util = require('util');
 
+var _bulletPush = function(token, text, cb) {
+    cb = cb || function() {};
+    var pusher = new PushBullet(token);
+
+    pusher.devices(function(error, response) {
+        if (Boolean(error)) {
+            cb(error, null);
+            return false;
+        }
+
+        response.devices.forEach(function(item, index){
+            pusher.note(item.iden, 'jsBotter', text, function(error, response) {
+                cb(error, response);
+            });
+        });
+
+    });
+};
+
 var _regPushToken = function(client, nick, message, raw) {
     var re = /^(!regpush) (\S+)/gi,
         commandArr = re.exec(message),
@@ -71,15 +90,54 @@ var _getPushToken = function(client, nick, message, raw) {
         });
 };
 
+
+//!push Elected привет
+var _push = function(client, nick, target, message, raw) {
+    var re = /^(!push) (\S+) (.+)/gi,
+        commandArr = re.exec(message);
+
+    if (commandArr === null) return false;
+    var nickToSend = commandArr[2],
+        text;
+
+    if (target === null) {
+        text = util.format('%s: %s', target, nick, commandArr[3]);
+    } else {
+        text = util.format('(%s) %s: %s', target, nick, commandArr[3]);
+    }
+
+    pushbulletModel.findOne({networkName: client._networkName, nick:nickToSend },
+        function (err, _model) {
+            if (err) {
+                client.say(nick, "Произошла ошибка "+JSON.stringify(err));
+                return false;
+            }
+
+            if (Boolean(_model) !== false) {
+                _bulletPush(_model.token,text, function(err) {
+                    if (Boolean(err)) {
+                        client.say(nick, "Произошла ошибка "+JSON.stringify(err));
+                        return false;
+                    }
+                    client.say(nick, "Сообщение успешно отправлено юзеру - " + nickToSend);
+                });
+            } else {
+                client.say(nick, util.format('Пользователь %s еще не обзавелся pushbullet', nickToSend) );
+            }
+
+        });
+
+};
+
 module.exports = {
     'message': function(nick, target, message, raw) {
-
+        _push(this, nick, target, message, raw);
     },
     'pm': function(nick, message, raw) {
 
         _regPushToken(this, nick, message, raw);
         _getPushToken(this, nick, message, raw);
-
+        _push(this, nick, null, message, raw);
 
     }
 };
